e6730dc4dcbd197a226a76a8d0cb6d4a70703a47
Hannes Naude <naude.jj@gmail.com>
2024-10-15 11:54:45 +0200

    Implement subsample logic
    Basic logic for subsampling (decimating) imagesets. Slow but it gets the job done for now. This was a a lambda  in a pre-typescript version. Need to get there again.

--- src/Subsample.tsx
+++ src/Subsample.tsx
@@ -1,9 +1,9 @@
-import React, { useState } from 'react';
+import React, { useState,useContext } from 'react';
 import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import { ImageSetDropdown } from './ImageSetDropDown';
-
+import { GlobalContext, ProjectContext } from './Context';
 interface SubsampleModalProps {
     show: boolean;
     handleClose: () => void;
@@ -18,11 +18,42 @@ const SubsampleModal: React.FC<SubsampleModalProps> = ({
     setSelectedImageSets 
 }) => {
     const [subsampleInterval, setSubsampleInterval] = useState<number>(2);
+    const { client } = useContext(GlobalContext)!;
+    const { project } = useContext(ProjectContext)!;
 
-    const handleSubmit = () => {
+    const  handleSubmit = async () => {
         // Implement the subsampling logic here
         console.log(`Subsampling with interval: ${subsampleInterval}`);
         console.log(`Selected Image Sets: ${selectedImageSets.join(', ')}`);
+        for (const imageSet of selectedImageSets) {
+            const allImages: { id: string, timestamp: number }[] = []
+            const { data } = await client.models.ImageSet.get({ id: imageSet })
+            const { name, images } = data
+            let prevNextToken: String | undefined = undefined;
+            do {
+                const result = await client.models.ImageSetMembership.imageSetMembershipsByImageSetId({ imageSetId: imageSet }, { selectionSet: ['image.id', 'image.timestamp'], nextToken: prevNextToken })
+                const { data, nextToken } = result
+                prevNextToken = nextToken
+                allImages.push(...data.map(({ image }) => image))
+            } while (prevNextToken)
+            //Sort images by timestamp
+            allImages.sort((a, b) => a.timestamp - b.timestamp)
+            //Subsample images
+            const subsampledImages = allImages.filter((_, index) => index % subsampleInterval === 0)
+            const newImageSetId=crypto.randomUUID()
+            for (const image of subsampledImages) {
+                await client.models.ImageSetMembership.create({
+                    imageSetId: newImageSetId,
+                    imageId: image.id,
+                })
+            }
+            await client.models.ImageSet.create({
+                id: newImageSetId,
+                projectId: project.id,
+                name: `${name}-sub-${subsampleInterval}`,
+            })
+            console.log(allImages)
+        }
         handleClose();
     };
 
