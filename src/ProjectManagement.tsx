3bed187dee48c21496352c9802dcccd43ccf3681
Hannes Naude <naude.jj@gmail.com>
2024-10-15 16:57:36 +0200

    Reload project management components when project is switched
    I saw that if I select an imageset and define a spatial subset, then toggle to another project, select another imageset and go define another spatial subset, then both imagesets are active inside the "Define Sptiotemporal subsets" modal.
    This is because both sets are listed in selectedSets, so the Spatial subset modal is actually doing the correct thing. We could add an effect to ImageSubsetManagement that watches the current project and resets all states when it changes, but we would have to do the same thing in all the other menegement components and keep these effects updated. The better solution is to force an actual reload (rather than simulating one) each time the project cahnges (because we never want to transfer state between projects). Tis is easily achieved by giving each of the menegement components a key that includes the current project id.

--- src/ProjectManagement.tsx
+++ src/ProjectManagement.tsx
@@ -3,7 +3,7 @@ import QueueManagement from './QueueManagement.tsx';
 import UserManagement from './UserManagement.tsx';
 import ImageSetManagement from './ImageSetManagement.tsx';
 import DefineCategories from './DefineCategories.tsx';
-import { GlobalContext } from './Context.tsx';
+import { GlobalContext,ProjectContext } from './Context.tsx';
 import { Management } from './UserContext.tsx';
 import DeleteImageSet from './DeleteImageSet.tsx';
 import FilesUploadComponent from './FilesUploadComponent.tsx';
@@ -12,17 +12,19 @@ import TaskManagement from './TaskManagement.tsx';
 import AnnotationSetManagement from './AnnotationSetManagement.tsx';
 
 const ProjectManagement = () => {
+  const { project } = useContext(ProjectContext)!;
 
   const {modalToShow, showModal} = useContext(GlobalContext)!;
   return (
       <div className="project-management">
         <Management>
-        <ImageSetManagement />
-        <DefineCategories />
-        <QueueManagement />
-        <TaskManagement />
-        <AnnotationSetManagement />
-        <UserManagement />
+        {/* We use this key to force a component reload when the project changes */}
+        <ImageSetManagement key={"imsets"+  project.id} />
+        <DefineCategories key={"cats"+project.id}/>
+        <QueueManagement key={"queue"+project.id}/>
+        <TaskManagement key={"tasks"+project.id}/>
+        <AnnotationSetManagement key={"asets"+project.id}/>
+        <UserManagement key={"users"+project.id}/>
         {/* <Rescan show={modalToShow=="rescan"} handleClose={()=>setModalToShow(null)}/> */}
         <DeleteImageSet
           show={modalToShow == "deleteImageSet"}
