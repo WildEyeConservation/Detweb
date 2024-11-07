6a0490c69a760d4e3c8785ab072693ee5d9caf32
jjnaude <naude.jj@gmail.com>
2024-11-07 14:26:50 +0200

    Various improvements to updateStats
    It seems that the remaining leaderboard issues can be traced to lambdas timing out and then being retried causing the same graphql update to be applied multiple times. I've changed the code structure to first accumulate all updates for the PK and then apply all the updates in parallel. This should buy us at least an order of magnitude scalewise.

--- amplify/functions/updateUserStats/handler.ts
+++ amplify/functions/updateUserStats/handler.ts
@@ -46,57 +46,87 @@ const logger = new Logger({
   serviceName: "dynamodb-stream-handler",
 });
 
-async function updateStats(input: any) {
+let stats = {}
+
+function accumulateStats(input: any) {
+    const setId = input.annotationSetId.S
+    const date = input.createdAt.S.split('T')[0]
+    const userId = input.owner.S.split('::')[1]
+    const projectId = input.projectId.S
+    const annotationCount = parseInt(input.annotationCount.N)
+    const timeTaken = parseFloat(input.timeTaken.N) || 0
+    const waitingTime = parseFloat(input.waitingTime.N) || 0
+    const sighting = (annotationCount > 0 ? 1 : 0)
+    const key = `${setId}-${date}-${userId}-${projectId}`
+    if (!stats[key]) {
+        stats[key] = stats[key] ||
+        {
+            setId,
+            date,
+            userId,
+            projectId,
+            observationCount: 0,
+            annotationCount: 0,
+            sightingCount: 0,
+            activeTime: 0,
+            searchTime: 0,
+            searchCount: 0,
+            annotationTime: 0,
+            waitingTime: 0
+        }
+    }
+    stats[key].observationCount += 1
+    stats[key].annotationCount += annotationCount
+    stats[key].sightingCount += sighting
+    stats[key].activeTime += timeTaken
+    stats[key].searchTime += (1-sighting) * timeTaken
+    stats[key].searchCount += (1 - sighting)
+    stats[key].annotationTime += sighting * timeTaken
+    stats[key].waitingTime += Math.max(waitingTime, 0)
+    logger.info(`Accumulated stats for ${key}`)
+}
+
+
+async function updateStats() {
+    const promises = [];
     try {
-        const setId = input.annotationSetId.S
-        const date = input.createdAt.S.split('T')[0]
-        const userId = input.owner.S.split('::')[1]
-        const projectId = input.projectId.S
-        const annotationCount = parseInt(input.annotationCount.N)
-        const timeTaken = parseFloat(input.timeTaken.N) || 0
-        const waitingTime = parseFloat(input.waitingTime.N) || 0
-        const sighting = (annotationCount > 0 ? 1 : 0)
-        const result = await client.graphql({
-            query: getUserStats,
-            variables: {
-                userId,
-                projectId,
-                setId,
-                date
+        for (const key in stats) {
+            const update = stats[key]
+            const result = await client.graphql({
+                query: getUserStats,
+                variables: {
+                    userId: update.userId,
+                    projectId: update.projectId,
+                    setId: update.setId,
+                    date: update.date
+                }
+            }) 
+            const statExists = result.data?.getUserStats
+            const stats = result.data?.getUserStats || { observationCount: 0, annotationCount: 0, sightingCount: 0, activeTime: 0, searchTime: 0, searchCount: 0, annotationTime: 0, waitingTime: 0 }
+            const variables={
+                input: {
+                    userId : update.userId,
+                    projectId: update.projectId,
+                    setId: update.setId,
+                    date: update.date,
+                    observationCount: stats.observationCount + 1,
+                    annotationCount: stats.annotationCount + update.annotationCount,
+                    sightingCount: (stats.sightingCount || 0) + update.sightingCount,
+                    activeTime: (stats.activeTime || 0) + update.activeTime,
+                    searchTime: (stats.searchTime || 0) + update.searchTime,
+                    searchCount: (stats.searchCount || 0) + update.searchCount,
+                    annotationTime: (stats.annotationTime || 0) + update.annotationTime,
+                    waitingTime:  (stats.waitingTime || 0) + update.waitingTime
+                }
             }
-        }).catch(error => {
-            logger.error('GraphQL getUserStats error:', { error: JSON.stringify(error, null, 2) });
-            throw error;
-        });
-        const stats = result.data?.getUserStats || {observationCount: 0, annotationCount: 0, sightingCount: 0, activeTime: 0, searchTime: 0, searchCount: 0, annotationTime: 0, waitingTime: 0}
-        const variables={
-            input: {
-                userId,
-                projectId,
-                setId,
-                date,
-                observationCount: stats.observationCount + 1,
-                annotationCount: stats.annotationCount + annotationCount,
-                sightingCount: (stats.sightingCount || 0) + sighting,
-                activeTime: (stats.activeTime || 0) + timeTaken,
-                searchTime: (stats.searchTime || 0) + (1-sighting) * timeTaken,
-                searchCount: (stats.searchCount || 0) + (1 - sighting),
-                annotationTime: (stats.annotationTime || 0) + sighting * timeTaken,
-                waitingTime:  (stats.waitingTime || 0) + Math.max(waitingTime, 0)
+            if (statExists) {
+                promises.push(client.graphql({query: updateUserStats, variables: variables}))
+            } else {
+                promises.push(client.graphql({query: createUserStats, variables: variables}))
             }
         }
-        if (!(variables.input.activeTime)) {
-            logger.warn('Missing stats', JSON.stringify(variables))
-        }
-        if (result.data?.getUserStats) {
-            logger.info(JSON.stringify(await client.graphql({
-                query: updateUserStats,variables
-            })))
-        } else {
-            logger.info(JSON.stringify(await client.graphql({
-                query: createUserStats,
-                variables})))
-        }
+        await Promise.all(promises)
+        logger.info(`Updated ${promises.length} stats`)
     } catch (error) {
         logger.error('Error in updateStats:', {
             error: JSON.stringify(error, null, 2),
@@ -119,9 +149,10 @@ export const handler: DynamoDBStreamHandler = async (event) => {
                 continue;
             }
             if (record.eventName === "INSERT") {
-                await updateStats(record.dynamodb.NewImage);
+                accumulateStats(record.dynamodb.NewImage)
             }
         }
+        await updateStats()
         return {
             batchItemFailures: [],
         };
