4d9010938e3416809bbdc05e34163d54e7eddd13
jjnaude <naude.jj@gmail.com>
2024-11-13 09:10:46 +0200

    location update
    - Blue rectangle should be hidden if user holds down shift.

--- src/Location.tsx
+++ src/Location.tsx
@@ -1,13 +1,14 @@
 import { useMap } from "react-leaflet";
-import { useContext,  useEffect } from "react";
+import { useContext,  useEffect, useState } from "react";
 import "leaflet-contextmenu/dist/leaflet.contextmenu.css";
 import { ImageContext } from "./Context";
 import L from "leaflet";
 import "leaflet-contextmenu";
 import { LocationType } from "./schemaTypes";
-
+import { isHotkeyPressed, useHotkeys } from "react-hotkeys-hook";
 export default function Location({x,y,width,height,confidence}: LocationType) {
   const { xy2latLng } = useContext(ImageContext)!;
+  const [enabled, setEnabled] = useState(true);
   width ||= 100
   height ||= 100
 
@@ -24,6 +25,14 @@ export default function Location({x,y,width,height,confidence}: LocationType) {
       callback: () => console.log("conf callback"),
     },
   ];
+
+  useHotkeys(
+    "Shift",
+    () => {
+      setEnabled(!isHotkeyPressed("Shift"));
+    },
+    { keyup: true, keydown: true },
+  );
 
   // if (user?.isAdmin) {
   //   contextMenuItems.push({
@@ -45,6 +54,7 @@ export default function Location({x,y,width,height,confidence}: LocationType) {
   // }
 
   useEffect(() => {
+    if (!enabled) return;
     const latLngBounds = xy2latLng(boundsxy) as unknown as L.LatLngBoundsLiteral;
     const rectangle = L.rectangle(latLngBounds, {
       // color: showTestCase && isTest ? "red" : "blue",
@@ -61,7 +71,7 @@ export default function Location({x,y,width,height,confidence}: LocationType) {
     return () => {
       map.removeLayer(rectangle);
     };
-  }, [map, boundsxy, contextMenuItems]);
+  }, [map, boundsxy, contextMenuItems, enabled]);
 
   return null;
 }
