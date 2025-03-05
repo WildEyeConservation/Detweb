ea19d06e83bbb333cea6146ccaeb19c2c7d68a41
EwanTrollip <ewan@innoventix.co.za>
2025-03-05 15:27:42 +0200

    Adapted TestPreloader to work with new testing schema

--- src/TestPreloader.tsx
+++ src/TestPreloader.tsx
@@ -5,53 +5,90 @@ import { PreloaderFactory } from './Preloader';
 import TestOutcomeModal from './TestOutcomeModal';
 import { fetchAllPaginatedResults } from './utils';
 import { TaskSelector } from './TaskSelector';
+import { Schema } from '../amplify/data/resource';
 
-//TODO: get type from schema
-type ConfigType = {
-    userId: string;
-    testType: string | null;
-    random: number | null;
-    deadzone: number | null;
-    interval: number | null;
-    projectId: string;
-    postTestConfirmation: boolean | null,
-    readonly id: string;
-    readonly createdAt: string;
-    readonly updatedAt: string;
-  };
-  
-  export default function TestPreloader({visible = true}: {visible: boolean}) {
-    const [config, setConfig] = useState<ConfigType | undefined>(undefined);
-    const [index, setIndex] = useState(1);
-    const [maxJobsCompleted, setMaxJobsCompleted] = useState(0);
-    const bufferClone = useRef<{locationId: string, annotationSetId: string, testPresetId: string}[]>([]);
-    const currentLocation = useRef<{locationId: string, annotationSetId: string, testPresetId: string} | null>(null);
-    const previousLocation = useRef<{locationId: string, annotationSetId: string, testPresetId: string} | null>(null);
-  
-    const { modalToShow, showModal, client } = useContext(GlobalContext)!;
-    const { currentPM, project } = useContext(ProjectContext)!;
-    //testing
-    const { jobsCompleted, setJobsCompleted, currentAnnoCount, setCurrentAnnoCount, unannotatedJobs, setUnannotatedJobs, isTesting, setIsTesting, isRegistering } = useContext(UserContext)!;
+export default function TestPreloader({
+  visible = true,
+}: {
+  visible: boolean;
+}) {
+  const [config, setConfig] = useState<
+    Schema['ProjectTestConfig']['type'] | undefined
+  >(undefined);
+  const [index, setIndex] = useState(1);
+  const [maxJobsCompleted, setMaxJobsCompleted] = useState(0);
+  const [testUser, setTestUser] = useState(false);
+  const bufferClone = useRef<
+    { locationId: string; annotationSetId: string; testPresetId: string }[]
+  >([]);
+  const currentLocation = useRef<{
+    locationId: string;
+    annotationSetId: string;
+    testPresetId: string;
+  } | null>(null);
+  const previousLocation = useRef<{
+    locationId: string;
+    annotationSetId: string;
+    testPresetId: string;
+  } | null>(null);
 
-    const { fetcher, fetchedLocation } = useTesting();
-  
-    const Preloader = useMemo(() => PreloaderFactory(TaskSelector), []);
-  
-    useEffect(() => {
-      async function setup() {
-          const {data: configs} = await client.models.UserTestConfig.testConfigByUserId({ userId: currentPM.userId });
-          const config = configs.find((c) => c.projectId === project.id);
-          if (config?.id) {
-              setConfig(config);
-          }
+  const { modalToShow, showModal, client } = useContext(GlobalContext)!;
+  const { currentPM, project } = useContext(ProjectContext)!;
+  //testing
+  const {
+    jobsCompleted,
+    setJobsCompleted,
+    currentAnnoCount,
+    setCurrentAnnoCount,
+    unannotatedJobs,
+    setUnannotatedJobs,
+    isTesting,
+    setIsTesting,
+    isRegistering,
+    myOrganizationHook,
+  } = useContext(UserContext)!;
+
+  const { fetcher, fetchedLocation } = useTesting();
+
+  const Preloader = useMemo(() => PreloaderFactory(TaskSelector), []);
+
+  useEffect(() => {
+    async function setup() {
+      const { data: config } = await client.models.ProjectTestConfig.get(
+        {
+          projectId: project.id,
+        },
+        {
+          selectionSet: [
+            'projectId',
+            'project.organizationId',
+            'postTestConfirmation',
+            'testType',
+            'random',
+            'deadzone',
+            'interval',
+          ],
+        }
+      );
+
+      if (config?.projectId) {
+        setConfig(config);
+
+        const shouldTest =
+          myOrganizationHook.data?.find(
+            (membership) =>
+              membership.organizationId === config.project.organizationId
+          )?.isTested ?? false;
+        setTestUser(shouldTest);
       }
-      
-      setup();
+    }
+
+    setup();
   }, []);
 
   useEffect(() => {
     if (fetchedLocation) {
-        bufferClone.current.push(fetchedLocation);
+      bufferClone.current.push(fetchedLocation);
     }
   }, [fetchedLocation]);
 
@@ -59,10 +96,12 @@ type ConfigType = {
   useEffect(() => {
     // Function to validate the test based on jobsCompleted
     async function validateTest() {
-      const userAnnotations = Object.entries(currentAnnoCount).filter(([_, annotations]) => annotations.length > 0);
-      
+      const userAnnotations = Object.entries(currentAnnoCount).filter(
+        ([_, annotations]) => annotations.length > 0
+      );
+
       if (isTesting && previousLocation.current) {
-         // Reset testing states
+        // Reset testing states
         setIsTesting(false);
         setUnannotatedJobs(0);
         setCurrentAnnoCount({});
@@ -77,7 +116,8 @@ type ConfigType = {
 
         // Fetch counts per category for the test location
         const categoryCounts = await fetchAllPaginatedResults(
-          client.models.LocationAnnotationCount.categoryCountsByLocationIdAndAnnotationSetId,
+          client.models.LocationAnnotationCount
+            .categoryCountsByLocationIdAndAnnotationSetId,
           {
             locationId: loc.locationId,
             annotationSetId: { eq: loc.annotationSetId },
@@ -100,24 +140,31 @@ type ConfigType = {
         );
 
         const boundsxy: [number, number][] = [
-          [location!.x - location!.width / 2, location!.y - location!.height / 2],
-          [location!.x + location!.width / 2, location!.y + location!.height / 2],
+          [
+            location!.x - location!.width / 2,
+            location!.y - location!.height / 2,
+          ],
+          [
+            location!.x + location!.width / 2,
+            location!.y + location!.height / 2,
+          ],
         ];
 
         // Filter annotations to those within the test location and count them per category
-        const annotationCounts: {[key: string]: number} = {};
+        const annotationCounts: { [key: string]: number } = {};
         for (const [categoryId, annotations] of userAnnotations) {
-            for (const annotation of annotations) {
-                const isWithin = 
-                    annotation.x >= boundsxy[0][0] && 
-                    annotation.y >= boundsxy[0][1] && 
-                    annotation.x <= boundsxy[1][0] && 
-                    annotation.y <= boundsxy[1][1];
+          for (const annotation of annotations) {
+            const isWithin =
+              annotation.x >= boundsxy[0][0] &&
+              annotation.y >= boundsxy[0][1] &&
+              annotation.x <= boundsxy[1][0] &&
+              annotation.y <= boundsxy[1][1];
 
-                if (isWithin) {
-                    annotationCounts[categoryId] = (annotationCounts[categoryId] || 0) + 1;
-                }
+            if (isWithin) {
+              annotationCounts[categoryId] =
+                (annotationCounts[categoryId] || 0) + 1;
             }
+          }
         }
 
         // Further filter user annotations based on test preset categories
@@ -126,9 +173,14 @@ type ConfigType = {
         );
 
         // Total count of annotations that were part of the defined categories
-        const totalUserCounts = filteredUserAnnotations.reduce((acc, [_, count]) => acc + count, 0);
+        const totalUserCounts = filteredUserAnnotations.reduce(
+          (acc, [_, count]) => acc + count,
+          0
+        );
         const totalTestCounts = categoryCounts
-          .filter((count) => categories.some((c) => c.categoryId === count.categoryId))
+          .filter((count) =>
+            categories.some((c) => c.categoryId === count.categoryId)
+          )
           .reduce((acc, count) => acc + (count.count as number), 0);
 
         const requiredAccuracy = testPreset?.accuracy ?? 0;
@@ -140,16 +192,21 @@ type ConfigType = {
           totalUserCounts >= minUserCounts && totalUserCounts <= maxUserCounts
             ? (totalUserCounts / totalTestCounts) * 100
             : 0;
-        
+
         const passedOnCategories = annotationAccuracy >= requiredAccuracy;
 
         // Total count of annotations regardless of category
-        const allUserCounts = Object.values(annotationCounts).reduce((acc, count) => acc + count, 0);
-        const passedOnTotal = allUserCounts >= totalTestCounts * (requiredAccuracy/100) && 
-          allUserCounts <= totalTestCounts * (2 - requiredAccuracy/100);
+        const allUserCounts = Object.values(annotationCounts).reduce(
+          (acc, count) => acc + count,
+          0
+        );
+        const passedOnTotal =
+          allUserCounts >= totalTestCounts * (requiredAccuracy / 100) &&
+          allUserCounts <= totalTestCounts * (2 - requiredAccuracy / 100);
 
         const { data: testResult } = await client.models.TestResult.create({
           userId: currentPM.userId,
+          projectId: project.id,
           testPresetId: loc.testPresetId,
           locationId: loc.locationId,
           annotationSetId: loc.annotationSetId,
@@ -167,7 +224,9 @@ type ConfigType = {
         // Track count of animals missed vs animals in tests, by category
         if (filteredUserAnnotations.length > 0) {
           for (const [categoryId, count] of filteredUserAnnotations) {
-            const categoryCount = categoryCounts.find((c) => c.categoryId === categoryId)?.count || 0;
+            const categoryCount =
+              categoryCounts.find((c) => c.categoryId === categoryId)?.count ||
+              0;
             await client.models.TestResultCategoryCount.create({
               testResultId: testResult.id,
               categoryId: categoryId,
@@ -178,14 +237,15 @@ type ConfigType = {
         } else {
           // User missed all animals; create entries with userCount = 0
           for (const category of categories) {
-            const categoryCount = categoryCounts.find((c) => c.categoryId === category.categoryId)?.count || 0;
+            const categoryCount =
+              categoryCounts.find((c) => c.categoryId === category.categoryId)
+                ?.count || 0;
             await client.models.TestResultCategoryCount.create({
               testResultId: testResult.id,
               categoryId: category.categoryId,
               userCount: 0,
               testCount: categoryCount,
             });
-
           }
         }
 
@@ -200,13 +260,18 @@ type ConfigType = {
 
         return;
       }
-    
+
       // This only counts unannotated jobs when continuing to the next job from the 'newest' (maxJobsCompleted) job.
       // If a user went back to a previous job, and annotated it, it would have no effect on the unannotatedJobs count.
       if (jobsCompleted > maxJobsCompleted) {
         setMaxJobsCompleted(jobsCompleted);
 
-        if (userAnnotations.reduce((acc, annotations) => acc + annotations[1].length, 0) > 0) {
+        if (
+          userAnnotations.reduce(
+            (acc, annotations) => acc + annotations[1].length,
+            0
+          ) > 0
+        ) {
           setUnannotatedJobs(0);
         } else {
           setUnannotatedJobs((j) => j + 1);
@@ -226,39 +291,45 @@ type ConfigType = {
       console.log('currentLocation updated to', currentLocation.current);
     }
   }, [index, fetcher, jobsCompleted]);
-  
-    useEffect(() => {
-      if (isRegistering || !fetcher || !config ||
-        config.testType === 'none' || 
-        config.testType === 'interval' && unannotatedJobs < config.interval! || 
-        config.testType === 'random' && Math.random() >= (config.random! / 100) || 
-        config.testType === 'random' && jobsCompleted <= config.deadzone!) {
-          setIsTesting(false);
-          return;
-      }
-  
-      setIsTesting(true);
-    }, [unannotatedJobs]);
-  
-    return (
-      <>
-      {fetcher &&
-        <Preloader 
-          index={index} 
-          setIndex={setIndex} 
+
+  useEffect(() => {
+    if (
+      isRegistering ||
+      !fetcher ||
+      !config ||
+      !testUser ||
+      config.testType === 'none' ||
+      (config.testType === 'interval' && unannotatedJobs < config.interval!) ||
+      (config.testType === 'random' && Math.random() >= config.random! / 100) ||
+      (config.testType === 'random' && jobsCompleted <= config.deadzone!)
+    ) {
+      setIsTesting(false);
+      return;
+    }
+
+    setIsTesting(true);
+  }, [unannotatedJobs]);
+
+  return (
+    <>
+      {fetcher && (
+        <Preloader
+          index={index}
+          setIndex={setIndex}
           fetcher={fetcher}
           visible={visible}
-          preloadN={2} 
-          historyN={1} 
+          preloadN={2}
+          historyN={1}
         />
+      )}
+      <TestOutcomeModal
+        show={
+          modalToShow === 'testFailedModal' || modalToShow === 'testPassedModal'
         }
-        <TestOutcomeModal 
-          show={
-            modalToShow === 'testFailedModal' || 
-            modalToShow === 'testPassedModal'
-          } 
-          onClose={() => {showModal(null)}} 
-        />
-      </>
-    )
-  }
+        onClose={() => {
+          showModal(null);
+        }}
+      />
+    </>
+  );
+}
