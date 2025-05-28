319eb430d718db9feaf13bd22bfd48ffc9c6fbca
EwanTrollip <ewan@innoventix.co.za>
2025-05-28 15:36:49 +0200

    Added container to PairLoader

--- src/PairLoader.tsx
+++ src/PairLoader.tsx
@@ -1,34 +1,58 @@
-import { useParams } from 'react-router-dom';
-import { useContext, useEffect, useState } from 'react';
-import { GlobalContext } from './Context';
-import { RegisterPair } from './RegisterPair';
+import { useParams } from "react-router-dom";
+import { useContext, useEffect, useState } from "react";
+import { GlobalContext } from "./Context";
+import { RegisterPair } from "./RegisterPair";
 import { array2Matrix, makeTransform } from "./utils";
-import { inv } from 'mathjs';
+import { inv } from "mathjs";
 
 export function PairLoader() {
-  const { image1Id,image2Id,selectedSet } = useParams();
+  const { image1Id, image2Id, selectedSet } = useParams();
   const [element, setElement] = useState<JSX.Element | null>(null);
   const { client } = useContext(GlobalContext)!;
-  
+
   useEffect(() => {
-    const loadPair = async (id1: string, id2: string, isReversed: boolean = false) => {
+    const loadPair = async (
+      id1: string,
+      id2: string,
+      isReversed: boolean = false
+    ) => {
       try {
-        const { data: { homography, image1, image2 } } = await client.models.ImageNeighbour.get(
+        const {
+          data: { homography, image1, image2 },
+        } = await client.models.ImageNeighbour.get(
           { image1Id: id1, image2Id: id2 },
-          { selectionSet: ['homography', 'image1.*', 'image2.*'] }
+          { selectionSet: ["homography", "image1.*", "image2.*"] }
         );
-        
+
         const homographyMatrix = array2Matrix(homography);
-        const transforms = homographyMatrix ? [
-          makeTransform(isReversed ? inv(homographyMatrix) : homographyMatrix),
-          makeTransform(isReversed ? homographyMatrix : inv(homographyMatrix))
-        ] : null;
-        
-        setElement(<RegisterPair 
-          transforms={transforms}
-          visible={true}
-          selectedSet={selectedSet!}
-          images={isReversed ? [image2, image1] : [image1, image2]} />);
+        const transforms = homographyMatrix
+          ? [
+              makeTransform(
+                isReversed ? inv(homographyMatrix) : homographyMatrix
+              ),
+              makeTransform(
+                isReversed ? homographyMatrix : inv(homographyMatrix)
+              ),
+            ]
+          : null;
+
+        setElement(
+          <div
+            style={{
+              width: "100%",
+              maxWidth: "1555px",
+              marginTop: "16px",
+              marginBottom: "16px",
+            }}
+          >
+            <RegisterPair
+              transforms={transforms}
+              visible={true}
+              selectedSet={selectedSet!}
+              images={isReversed ? [image2, image1] : [image1, image2]}
+            />
+          </div>
+        );
       } catch (error) {
         if (!isReversed) {
           // If first attempt fails, try with reversed parameters
@@ -41,6 +65,6 @@ export function PairLoader() {
       loadPair(image1Id, image2Id);
     }
   }, [image1Id, image2Id, selectedSet]);
-    
+
   return element;
 }
