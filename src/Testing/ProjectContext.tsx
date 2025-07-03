b2eb00f6d15218a20648635a4c8da84d3628a25f
EwanTrollip <ewan@innoventix.co.za>
2025-07-03 17:01:46 +0200

    Added edit locations modal

---
+++ src/Testing/ProjectContext.tsx
@@ -0,0 +1,50 @@
+import { Project, Management } from '../UserContext';
+import { Schema } from '../../amplify/data/resource';
+import { UserContext, GlobalContext } from '../Context';
+import { useContext, useEffect, useState } from 'react';
+
+export default function ProjectContext({
+  children,
+  surveyId,
+}: {
+  children: React.ReactNode;
+  surveyId: string;
+}) {
+  const { user } = useContext(UserContext)!;
+  const { client } = useContext(GlobalContext)!;
+  const [currentPM, setCurrentPM] = useState<
+    Schema['UserProjectMembership']['type'] | null
+  >(null);
+
+  useEffect(() => {
+    const fetchCurrentPM = async () => {
+      if (!surveyId || !user.userId) {
+        return;
+      }
+
+      const {
+        data: [currentPM],
+      } =
+        await client.models.UserProjectMembership.userProjectMembershipsByUserId(
+          {
+            userId: user.userId,
+          },
+          {
+            filter: {
+              projectId: {
+                eq: surveyId,
+              },
+            },
+          }
+        );
+      setCurrentPM(currentPM);
+    };
+    fetchCurrentPM();
+  }, [user.userId, surveyId]);
+
+  return currentPM ? (
+    <Project currentPM={currentPM}>
+      <Management>{children}</Management>
+    </Project>
+  ) : null;
+}
