667a4e8e716f540708de32ef91d0f8005636f85a
Ewan Trollip <ewantrollip@gmail.com>
2026-08-06 09:08:45 +0200

    fix: prevent duplicate queue observation counts

--- src/useCreateObservation.tsx
+++ src/useCreateObservation.tsx
@@ -108,14 +108,6 @@ export default function useCreateObservation(props: UseCreateObservationProps) {
           group: project.organizationId,
         });
 
-        if (queueId) {
-          client.mutations
-            .incrementQueueCount({ id: queueId })
-            .catch((err: unknown) =>
-              console.error('Failed to increment observedCount', err)
-            );
-        }
-
         setAcked(true);
       }
 
